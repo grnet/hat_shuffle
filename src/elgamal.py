8e403640377ef4a11cdcaac02717e1e3c7392c90
Panos Louridas <louridas@gmail.com>
2017-02-02 17:47:01 +0200

    add ElGamal system

---
+++ src/elgamal.py
@@ -0,0 +1,48 @@
+from bplib.bp import BpGroup
+
+def gen_params_bp_g2(nid=713):
+    G = BpGroup()
+    g = G.gen2()
+    o = G.order()
+    return (G, g, o)
+
+def key_gen(params):
+    _, g, o = params
+    priv = o.random()
+    pub = (g, priv * g)
+    return (pub, priv)
+    
+def enc(pub, m):
+    pk1, pk2 = pub
+    o = pk1.group.order()
+    r = o.random()
+    a = r * pk1
+    b = m * pk1 + r * pk2
+    return (a, b)
+
+def dec(priv, c, table):
+    a, b = c
+    v = b + (-priv * a)
+    return table[v]
+
+def make_table(g, n):
+    table = {}
+    for i in range(n):
+        elem = (i * g)
+        table[elem] = i
+    return table
+
+def test_encdec(params):
+    table = make_table(params[1], 1000)
+    G, g, o = params
+    pub, priv = key_gen(params)
+    g2 = G.gen2()
+    import random
+    ps = random.sample(range(1000), 100)
+    for i in range(100):
+        c = enc(pub, ps[i])
+        assert(dec(priv, c, table) == ps[i])
+
+if __name__ == '__main__':
+    print("Testing BP group G2...")
+    test_encdec(gen_params_bp_g2())
