278ef49c4fe33f06775a534b27f0abb8de8b6468
StefanosChaliasos <t8140192@dias.aueb.gr>
2018-03-20 12:31:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprocps.so
 cd libffpy
-rm -fr build/
-rm libffpy.so
 sudo python setup.py install &
 wait
 cp build/lib.linux-x86_64-2.7/libffpy.so  ./
