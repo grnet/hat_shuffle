5278e5c830ab772ecdc794e9375b6eff623accfa
StefanosChaliasos <t8140192@dias.aueb.gr>
2018-03-20 12:33:37 +0200

    Remove sudo

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libprocps.so
 cd libffpy
-sudo python setup.py install &
+python setup.py install &
 wait
 cp build/lib.linux-x86_64-2.7/libffpy.so  ./
 python demo.py 10
